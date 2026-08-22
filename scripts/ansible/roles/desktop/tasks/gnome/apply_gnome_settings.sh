476d8c790f50235bb04801ceaf5a1771ee1c7199
Alessio Attilio <alessio.attilio@protonmail.com>
2026-08-22 20:56:36 +0200

    refactor(desktop): remove GNOME extensions and EGO config
    Signed-off-by: Alessio Attilio <alessio.attilio@protonmail.com>

--- scripts/ansible/roles/desktop/tasks/gnome/apply_gnome_settings.sh
+++ scripts/ansible/roles/desktop/tasks/gnome/apply_gnome_settings.sh
@@ -10,9 +10,3 @@ gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface enable-animations false
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-gsettings set org.gnome.shell.extensions.dash-to-dock pressure-threshold 100
-gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
-gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.25
